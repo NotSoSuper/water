8308e57575b73b367c72e88cbb7d6e85ae1e13d7
Zeyla Hellyer <zeylahellyer@gmail.com>
2018-02-25 06:37:44 -0800

    Add 'toString' implementation on Snowflake
    This allows the use of simply using the snowflake in template literals,
    rather than accessing its `value` property.

--- src/Abstracts/Snowflake.ts
+++ src/Abstracts/Snowflake.ts
@@ -1,9 +1,15 @@
 export default class Snowflake {
-    constructor(public value: string) {}
+    constructor(public value: string) {
+    }
+
     static fromJSON(value: string) {
         return this.prototype.constructor.call(value);
     }
     toJSON() {
+        return this.value;
+    }
+
+    toString(): string {
         return this.value;
     }
 }
